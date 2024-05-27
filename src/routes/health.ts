6ffb233a2163d29dd146ff156709afbffc9e5234
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-05-27 14:12:47 +0200

    added health check to the health route

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,7 +1,21 @@
 import { eventHandler } from "h3";
+import { getAllIntegrationIds } from "../integrations/load";
+import { verifyIntegrity } from "../check";
+import { ORIGIN } from "../paths";
 
-export default eventHandler(async (event) => ({
-    path: event.path,
-    time: new Date(),
-    online: true,
-}))
+export default eventHandler(async () => {
+    const integrations = await getAllIntegrationIds()
+    const errors = (await Promise.all(integrations.map(i => verifyIntegrity(i)))).flat()
+
+    return ({
+        time: new Date(),
+        app: {
+            origin: ORIGIN
+        },
+        integrations: {
+            total: integrations.length,
+            success: errors.length === 0,
+            errors
+        }
+    })
+})
