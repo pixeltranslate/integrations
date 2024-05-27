71b03f76e3b3ba325fadc5452faf4a67e650a2c0
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-05-27 12:54:36 +0200

    finished get all integrations route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import { createApp, createRouter } from "h3";
 
 import HealthRouteHandler from './routes/health'
-import IntegrationByIdRouteHandle from './routes/integration'
+import IntegrationsListRouteHandler from './routes/list'
+import IntegrationByIdRouteHandler from './routes/integration'
 import IconRouteHandler from './routes/icon'
 
 export const app = createApp();
@@ -9,6 +10,7 @@ export const app = createApp();
 const router = createRouter();
 app.use(router);
 
-router.get("/", HealthRouteHandler);
-router.get("/:integration", IntegrationByIdRouteHandle)
+router.get("/", IntegrationsListRouteHandler)
+router.get("/:integration", IntegrationByIdRouteHandler)
 router.get("/:integration/icon", IconRouteHandler)
+router.get("/health", HealthRouteHandler);
--- src/integrations/load.ts
+++ src/integrations/load.ts
@@ -1,6 +1,7 @@
-// import { resolve, join, basename, extname } from 'node:path'
+import { join, basename, extname } from 'node:path'
 import { promises, existsSync } from 'node:fs'
 import * as yml from 'js-yaml'
+import { globby } from 'globby'
 import { Integration } from '../types'
 import { integrationsDir } from '../paths'
 
@@ -12,11 +13,9 @@ export async function loadIntegrationFromPath (id?: string) {
     return yml.load(await promises.readFile(path, 'utf8')) as Integration
 }
 
-/*
-export async function readModules() {
-    const globPattern = join(INTEGRATION_FOLDER, '*.yml').replace(/\\/g, '/')
+export async function loadIntegrations() {
+    const globPattern = join(integrationsDir, '*.yml').replace(/\\/g, '/')
     const names = (await globby(globPattern)).map(p => basename(p, extname(p))).filter(_ => _)
 
-    return Promise.all(names.map(n => getModule(n))).then(modules => modules.filter(m => m.name))
+    return Promise.all(names.map(n => loadIntegrationFromPath(n))).then(integrations => integrations.filter(i => !!i))
 }
-*/
