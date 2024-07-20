445b31c11627617ed961760329d6381fbd423062
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-07-20 20:26:13 +0200

    fix: checking script

--- scripts/integrity-check.ts
+++ scripts/integrity-check.ts
@@ -1,4 +1,4 @@
-import { verifyIntegrationsIntegrity } from './src/check'
+import { verifyIntegrationsIntegrity } from '../server/integrations/check'
 
 verifyIntegrationsIntegrity().catch((err) => {
   console.error(err)
