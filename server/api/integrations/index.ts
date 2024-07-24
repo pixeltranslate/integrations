030dd0c691d4303a1873c6324f44b47b73936739
Zoey <zoeykaiser8@gmail.com>
2024-07-24 18:28:48 +0200

    fix: asset that integrationId is a string

--- server/api/integrations/index.ts
+++ server/api/integrations/index.ts
@@ -6,7 +6,7 @@ export default defineEventHandler(async () => {
   const integrations = await Promise.all(integrationFiles.map(async (fileKey) => {
     const _integration = await useStorage('assets:integrations').getItem(fileKey)
     const integrationId = fileKey.split('.')[0]
-    return parseYMLToIntegration(integrationId, _integration)
+    return parseYMLToIntegration(integrationId as string, _integration)
   }))
 
   return integrations
